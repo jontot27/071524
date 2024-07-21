ec7dba52e5407e13a8fcb0483644553fd17477c6
jontot27 <j.a.ballesterosjr@gmail.com>
2024-07-21 19:54:02 +0800

    Creating Multiple States In React

--- src/assets/Counter.jsx
+++ src/assets/Counter.jsx
@@ -2,18 +2,29 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(1);
   function addClick() {
-    setCount(count + 1);
+    setCount(count + number);
   }
   function subClick() {
-    setCount(count - 1);
+    setCount(count - number);
+  }
+  function toAdd() {
+    setNumber(number + 1);
+  }
+  function toSub() {
+    setNumber(number - 1);
   }
 
   return (
     <div>
       <h1>Count value is:{count}</h1>
-      <button onClick={addClick}>Add</button>
-      <button onClick={subClick}>Subtract</button>
+      <button onClick={addClick}>+</button>
+      <button onClick={subClick}>-</button>
+
+      <h1>Added/Subrated Value:{number}</h1>
+      <button onClick={toAdd}>Value to be Added</button>
+      <button onClick={toSub}>Value to be Subtracted</button>
     </div>
   );
 }
